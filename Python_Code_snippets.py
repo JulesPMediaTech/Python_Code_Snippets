89562723c5385c64241e965d9fcb79472a57d7f2
julespyke <info@golively.tv>
2025-04-14 01:56:55 +0100

    Snippets of code useful for transplanting into projects includes decorators, functions and classes
    ADDED..
    @singleton decorator
    string-to-boolean handler function
    performance timer decorator

---
+++ Python_Code_snippets.py
@@ -0,0 +1,33 @@
+def stringToBool(var):
+    """Convert qualifying string variables to Boolean"""
+    _allowed = ["True", "true", "False", "false"]
+    if isinstance(var,bool) : return var
+    if not isinstance(var,str):
+        raise TypeError(f"{type(var)} Variable must be a string")
+    if var not in _allowed:
+        raise TypeError("Variable must contain either 'True' or 'False' values")
+    return (var == "True" or var == "true")
+
+
+
+import time
+def performance_timer(func):
+    """decorator for timing functions"""
+    def wrapper(*args,**kwargs):
+        start_time = time.perf_counter()
+        result = func(*args,**kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print (f"Function: {func.__name__!r} took {elapsed_time:.4f} seconds")
+        return result
+    return wrapper
+
+
+def singleton (class_instance):
+    """Singleton class decorator"""
+    instances = {}
+    def get_instance (*args, **kwargs):
+        if class_instance not in instances:
+            instances[class_instance] = class_instance(*args,**kwargs)
+        return instances[class_instance]
+    return get_instance
---
+++ README.md
@@ -0,0 +1,10 @@
+# <ins> Python Code Snippets </ins>
+
+<ins> useful code to transplant / implement into projects </ins>
+
+Index...... \
+Performance Timer as a function decorator \
+Singleton Pattern \
+String to Boolean handler function
+
+
